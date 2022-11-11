1aebaff32859f08cfa74f7bad870cef0d28a4c6f
mireyachaparro <mireyacn99@gmail.com>
2022-11-11 20:16:41 +0100

    Update header

--- src/features/store/components/sample/sample.tsx
+++ src/features/store/components/sample/sample.tsx
@@ -1,34 +1,34 @@
 import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { rootState } from '../../../../infrastructure/store/store';
-import * as ac from '../../reducer/action.creators';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { rootState } from '../../../../infrastructure/store/store';
+// import * as ac from '../../reducer/action.creators';
 
-export function Sample() {
-    const tasks = useSelector((state: rootState) => state.tasks); //l aparte del estado que queremos, qué rama queremos del store
+// export function Sample() {
+//     const tasks = useSelector((state: rootState) => state.tasks); //l aparte del estado que queremos, qué rama queremos del store
 
-    const dispatcher = useDispatch();
+//     const dispatcher = useDispatch();
 
-    useEffect(() => {
-        dispatcher(
-            ac.loadActionCreate([
-                {
-                    id: '1',
-                    title: 'terminar redux',
-                    responsible: 'pepe',
-                    isComplete: false,
-                },
-            ])
-        );
-    }, [dispatcher]);
+//     useEffect(() => {
+//         dispatcher(
+//             ac.loadActionCreate([
+//                 {
+//                     id: '1',
+//                     title: 'terminar redux',
+//                     responsible: 'pepe',
+//                     isComplete: false,
+//                 },
+//             ])
+//         );
+//     }, [dispatcher]);
 
-    return (
-        <>
-            {tasks.map((item) => (
-                <>
-                    <p key={item.id}>Task: {item.title}</p>
-                    <p>Responsible: {item.responsible}</p>
-                </>
-            ))}
-        </>
-    );
-}
+//     return (
+//         <>
+//             {tasks.map((item) => (
+//                 <>
+//                     <p key={item.id}>Task: {item.title}</p>
+//                     <p>Responsible: {item.responsible}</p>
+//                 </>
+//             ))}
+//         </>
+//     );
+// }
