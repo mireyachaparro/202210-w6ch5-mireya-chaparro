8bb624b9d6e73dbfd2c3c625a380aeaca6196aa2
mireyachaparro <mireyacn99@gmail.com>
2022-11-12 18:08:52 +0100

    Add onsale render

--- src/features/home/components/onsale/onsale.tsx
+++ src/features/home/components/onsale/onsale.tsx
@@ -1,13 +1,21 @@
+import { useProduct } from '../../../../infrastructure/hooks/custom.hooks';
+
 export function OnSale() {
     const title = 'On Sale';
+    const { products } = useProduct();
+    console.log(products);
+    const result = products.filter((item) => item.onsale);
+    console.log(result);
     return (
         <>
             <header className="onsale">
                 <h1 className="header__h1">{title}</h1>
             </header>
-            <main className="main">
-                <p>aqui aparece la lista de onsale</p>
-            </main>
+            <p>aqui aparece la lista de onsale</p>
+
+            {result.map((item) => (
+                <p key={item.id}>{item.name}</p>
+            ))}
         </>
     );
 }
